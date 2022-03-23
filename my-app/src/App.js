df4713857e9d6490a53b42dd88f0e4643eda3d83
Rishab-Rana <rishabrana228@gmail.com>
2022-03-23 16:58:21 +0530

    added-navigation-bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,11 @@
-
+import 'bootstrap/dist/css/bootstrap.css';
+import Header from './header';
+import FirstComponent from './FirstContainer'
 function App() {
   return (
     <div>
-      
+      <Header />
+      <FirstComponent />
     </div>
   );
 }
---
+++ my-app/src/FirstContainer.js
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+function FirstComponent(){
+    return(
+        <div>
+            <div class="container">
+  <div class="row">
+    <div class="col">
+      <img src="../public/Screenshot 2022-01-12 144756.png"></img>
+    </div>
+    <div class="col">
+      
+    </div>
+    
+  </div>
+</div>
+        </div>
+    );
+}
+
+export default FirstComponent;
